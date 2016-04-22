dc0f9204399902abdc15631316dfbe9b87879f6e
Daniel Stenberg <daniel@haxx.se>
2016-04-22 23:58:22 +0200

    readurl: add source code header

--- examples/readurl.c
+++ examples/readurl.c
@@ -1,3 +1,20 @@
+/***************************************************************************
+ * fcurl
+ *
+ * Copyright (C) 2016, Daniel Stenberg, <daniel@haxx.se>
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution. The terms
+ * are also available at https://curl.haxx.se/docs/copyright.html.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the COPYING file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
 #include <stdio.h>
 #include <fcurl.h>
 
